549a86bce1a69324241fca6394fad31818f8f3d3
Luana Fernanda Rodrigues dos Reis <110433841+Luanareis123@users.noreply.github.com>
2022-12-14 13:27:37 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; private set; }
+        public double Salario { get; protected set; }
+
+        public static int TotalDeFuncionario { get; private set; }
+
+        public virtual double GetBonificacao()
+        {
+            return this.Salario * 0.10;
+        }
+
+        public Funcionario(string cpf, double salario)
+        {
+            this.Salario = salario;
+            this.Cpf = cpf;
+            TotalDeFuncionario++;
+            Console.WriteLine("Criando um Funcionario");
+        }
+     
+        public virtual void AumentarSalario()
+        {
+            this.Salario *= 0.1;
+        }
+}    }   
