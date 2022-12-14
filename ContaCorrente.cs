9766b830bc3f1438a07526b6ae0665fbce593487
Luana Fernanda Rodrigues dos Reis <110433841+Luanareis123@users.noreply.github.com>
2022-12-14 13:25:21 -0300

    Add files via upload

---
+++ ContaCorrente.cs
@@ -0,0 +1,85 @@
+﻿using bytebank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank.Contas
+{
+    public class ContaCorrente
+    { 
+        private int numero_agencia;
+        public int Numero_agencia
+        {
+            get { return this.numero_agencia; }
+            private set
+            {
+                if (value > 0 )
+                {
+                    this.numero_agencia = value;
+                }
+            }
+            
+        }
+        public string conta { get; set; }
+        private double saldo = 100;
+
+        public Cliente Titular { set; get; }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+        }
+        public bool Sacar(double valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+
+        }
+        // Set (Definir valor) e Get (Obter valor)
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+        public ContaCorrente(int numero_agencia, string numero_conta)
+        {
+            this.numero_agencia = numero_agencia;
+            this.conta = numero_conta;
+        }
+        
+    }
+}
